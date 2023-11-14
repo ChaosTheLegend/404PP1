b546f0ba803d3234b265cf544ddd6968a5045c91
Yakimov Ivan <legendthechaos@gmail.com>
2023-11-14 19:36:53 +0600

    Added quiz 3 problem

---
+++ Quiz3/A.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string s;
+    getline(cin, s);
+
+    int length = s.length();
+
+    string output[length];
+
+    int startWord = 0;
+    int endWord = 0;
+    int k = 0;
+    bool hasNumber = false;
+
+    for (int i = 0; i < length; ++i) {
+        if(s[i] == ' '){
+            endWord = i-1;
+            int wordLength = endWord - startWord + 1;
+            string word = s.substr(startWord, wordLength);
+
+            if(!hasNumber) {
+                output[k] = word;
+                k++;
+            }
+
+            hasNumber = false;
+            startWord = i+1;
+            continue;
+        }
+
+        if(isdigit(s[i])){
+            hasNumber = true;
+        }
+    }
+
+    int wordLength = s.length() - startWord;
+    string word = s.substr(startWord, wordLength);
+    if(!hasNumber) {
+        output[k] = word;
+        k++;
+    }
+
+    for (int i = 0; i < k; ++i) {
+        cout << output[i] << endl;
+    }
+
+
+}
